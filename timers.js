85e5d09aeae62b4096ac6227e4afa59751f24e30
Alex Gomez <alexgomez_26@outlook.es>
2024-09-14 12:13:10 -0500

    timers

---
+++ timers.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+    console.log("hola mundo espera y loop de ejecuciones")
+}, 2000); //milisegundos
+
+
+setTimeout(() => {
+    console.log("hola mundo 2 espera ejecuta y termina")
+}, 5000); //milisegundos
