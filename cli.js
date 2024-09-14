aaaf7a47280e5d25e4388f7e7a8dd236da5825da
Alex Gomez <alexgomez_26@outlook.es>
2024-09-14 16:11:30 -0500

    modulos de terceros npm

---
+++ cli.js
@@ -0,0 +1,8 @@
+const colors = require('colors')
+
+console.log("facebook.com".bgBlue)
+console.log("google.com".bgRed)
+console.log("android.com".bgGreen)
+
+//npm install agregar
+//npm remove quitar dependencia/module
