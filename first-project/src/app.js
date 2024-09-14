27c480ddc68a11b4ab0427f7c901d9bd25f5f15f
Alex Gomez <alexgomez_26@outlook.es>
2024-09-14 16:32:43 -0500

    scripts package json y nodemon, dependecia de desarrollo

---
+++ first-project/src/app.js
@@ -0,0 +1,6 @@
+const colors = require("colors")
+
+console.log("Hola mundo mi primer proyecto con nodemon".rainbow)
+
+//npm run script //ejecucion de scripts
+//npm i dependencia -D //lo marca como dependencia para desarrollo
