65304c6a74bb40a46a67d7a15b20c79daf568c17
Alex Gomez <alexgomez_26@outlook.es>
2024-09-15 13:51:34 -0500

    util node para convertir callback en promesas

--- practica1/promise.js
+++ practica1/promise.js
@@ -1,17 +1,23 @@
 //Codigo async utiliza promesas
 const { readFile } = require("fs");
+const {promisify} = require('util')
+
 
 //funcion que retorna una promesa
-const getText = (pathFile) => {
-  return new Promise(function (resolve, reject) {
-    readFile(pathFile, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
-  });
-};
+//ESTO SE LO REEMPLAZA POR FUNCION UTILITARIA QUE CONVIERTE EL CALLBACK EN UNA PROMESA
+//const getText = (pathFile) => {
+//  return new Promise(function (resolve, reject) {
+//    readFile(pathFile, "utf-8", (err, data) => {
+//      if (err) {
+//        reject(err);
+//      }
+//      resolve(data);
+//    });
+//  });
+//};
+//reemplazo por util
+const readFilePromise = promisify(readFile)
+
 
 //con las promesas podemos utilizar las funciones post pejecucion then y catch
 //Codigo antiguo
@@ -25,12 +31,12 @@ const getText = (pathFile) => {
 //await especifica que el codigo a ajecutar es asincrono y necesita estar dentro de una funcion marcada con async
 async function read() {
   try {
-    const result = await getText("./practica1/data/first.txt");
-    const result2 = await getText("./practica1/data/second.txt");
-    const result3 = await getText("./practica1/data/third.txt");
-    const result4 = await getText("./practica1/data/fourth.txt");
+    const result = await readFilePromise("./practica1/data/first.txt", 'utf-8');
+    const result2 = await readFilePromise("./practica1/data/second.txt", 'utf-8');
+    const result3 = await readFilePromise("./practica1/data/third.txt", 'utf-8');
+    const result4 = await readFilePromise("./practica1/data/fourth.txt", 'utf-8');
 
-    throw new Error('Error controlado pruebas') //ERROR CONTROLADO
+    //throw new Error('Error controlado pruebas') //ERROR CONTROLADO
     console.log(result);
     console.log(result2);
     console.log(result3);
